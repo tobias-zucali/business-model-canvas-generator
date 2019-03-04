7020c403d6bf75ac5c27dcc9f1aefcb395f707b2
Tobias Zucali <tobias@zucali.at>
2019-03-04 13:45:55 +0100

    add splitMarkdown utils

---
+++ src/utils/splitMarkdown/index.js
@@ -0,0 +1,142 @@
+
+const mainHeaderRegex = /^\s*#\s*(.*)$/
+const anyHeaderRegex = /^\s*#(.*)$/
+const propertyRegex = /^\s*(\S+)\s*:(.*)$/
+const sectionHeaderRegex = /^\s*##([^{]*){([^}]*)}(.*)$/
+const placeholderRegex = /\[(.*)\S*\]/
+
+export default function splitMarkdown(content) {
+  const lines = content.split('\n')
+
+  const {
+    header,
+    index: headerIndex,
+  } = findHeader(lines)
+
+  const {
+    props,
+    lastIndex: lastPropIndex,
+  } = findProps(lines, headerIndex + 1)
+
+  const {
+    sections,
+  } = findSections(lines, lastPropIndex + 1)
+
+  return {
+    header,
+    props,
+    sections,
+  }
+}
+
+export function findHeader(lines) {
+  let currentIndex = 0
+
+  while (currentIndex < lines.length) {
+    const currentLine = lines[currentIndex]
+    const headerMatch = currentLine.match(mainHeaderRegex)
+
+    if (headerMatch) {
+      const [/* fullMatch */, header] = headerMatch
+      return {
+        header: header.trim(),
+        index: currentIndex,
+      }
+    }
+    currentIndex += 1
+  }
+  return {
+    header: '',
+    index: -1,
+  }
+}
+
+export function findProps(lines, startIndex) {
+  let currentIndex = startIndex
+  const props = {}
+
+  while (currentIndex < lines.length) {
+    const currentLine = lines[currentIndex]
+    const propertyMatch = currentLine.match(propertyRegex)
+
+    if (propertyMatch) {
+      const [/* fullMatch */, key, value] = propertyMatch
+      props[key.trim()] = value.trim()
+    } else if (currentLine.match(anyHeaderRegex)) {
+      break
+    }
+    currentIndex += 1
+  }
+
+  return {
+    props,
+    lastIndex: currentIndex - 1,
+  }
+}
+
+export function findSections(lines, startIndex) {
+  let currentIndex = startIndex
+  const sections = {}
+
+  while (currentIndex < lines.length) {
+    let currentLine = lines[currentIndex]
+    const sectionHeaderMatch = currentLine.match(sectionHeaderRegex)
+
+    if (sectionHeaderMatch) {
+      const [/* fullMatch */, preHeader, key, postHeader] = sectionHeaderMatch
+
+      const header = `${preHeader.trim()} ${postHeader.trim()}`.trim()
+
+      const {
+        placeholder,
+        index: placeholderIndex,
+      } = findPlaceholderText(lines, currentIndex + 1)
+
+      currentIndex = placeholderIndex + 1 || currentIndex + 1
+      const body = []
+
+      while (currentIndex < lines.length && !(lines[currentIndex + 1] || '').match(sectionHeaderRegex)) {
+        currentLine = lines[currentIndex]
+        body.push(currentLine)
+        currentIndex += 1
+      }
+
+      sections[key.trim()] = {
+        header,
+        placeholder,
+        body: body.join('\n').trim(),
+      }
+    }
+    currentIndex += 1
+  }
+  return {
+    sections,
+  }
+}
+
+export function findPlaceholderText(lines, startIndex) {
+  let currentIndex = startIndex
+  const sections = {}
+
+  while (currentIndex < lines.length) {
+    const currentLine = lines[currentIndex]
+    const placeholderMatch = currentLine.match(placeholderRegex)
+
+    if (placeholderMatch) {
+      const [/* fullMatch */, placeholder] = placeholderMatch
+      return {
+        placeholder: placeholder.trim(),
+        index: currentIndex,
+      }
+    } else if (currentLine.trim() !== '') {
+      return {
+        placeholder: '',
+        index: -1,
+      }
+    }
+    currentIndex += 1
+  }
+  return {
+    sections,
+  }
+}
---
+++ src/utils/splitMarkdown/tests/index.test.js
@@ -0,0 +1,257 @@
+import splitMarkdown, { findHeader, findProps, findSections, findPlaceholderText } from '../index'
+
+
+const testContent = `
+# Your Business
+date: 2012-12-12
+version: 1
+by: My beloved ones
+for: 
+
+## Purpose {purpose}
+[Without a clear purpose, how will we know if a model is good or not?]
+
+### This is an initial text!
+With multiline!!!
+
+## Customer Segments {customer-segments}
+[To build an effective business model, a company must identify which customers it tries to serve.]
+
+## Value Propositions {value-propositions}
+[The collection of products and services a business offers to meet the needs of its customers.]
+`
+
+describe('splitMarkdown', () => {
+  it('splits markdown', () => {
+    expect(
+      splitMarkdown(testContent)
+    ).toEqual(
+      {
+        header: 'Your Business',
+        props: {
+          by: 'My beloved ones',
+          date: '2012-12-12',
+          for: '',
+          version: '1',
+        },
+        sections: {
+          'customer-segments': {
+            body: '',
+            header: 'Customer Segments',
+            placeholder: 'To build an effective business model, a company must identify which customers it tries to serve.',
+          },
+          purpose: {
+            body: '### This is an initial text!\nWith multiline!!!',
+            header: 'Purpose',
+            placeholder: 'Without a clear purpose, how will we know if a model is good or not?',
+          },
+          'value-propositions': {
+            body: '',
+            header: 'Value Propositions',
+            placeholder: 'The collection of products and services a business offers to meet the needs of its customers.',
+          },
+        },
+      }
+    )
+  })
+
+  describe('findHeader', () => {
+    it('finds the header in the frist line', () => {
+      expect(
+        findHeader(['# oh my header!'])
+      ).toEqual(
+        {
+          header: 'oh my header!',
+          index: 0,
+        }
+      )
+    })
+
+    it('trims white space', () => {
+      expect(
+        findHeader(['#   oh my header!    '])
+      ).toEqual(
+        {
+          header: 'oh my header!',
+          index: 0,
+        }
+      )
+    })
+
+    it('finds the header in another line', () => {
+      expect(
+        findHeader(['', '', '# oh my header!', '## ignore this please!'])
+      ).toEqual(
+        {
+          header: 'oh my header!',
+          index: 2,
+        }
+      )
+    })
+
+    it('returns index -1 if no header is found', () => {
+      expect(
+        findHeader(['', ''])
+      ).toEqual(
+        {
+          header: '',
+          index: -1,
+        }
+      )
+    })
+  })
+
+  describe('findProps', () => {
+    it('finds props', () => {
+      expect(
+        findProps(['color: red', 'severity: hell'], 0)
+      ).toEqual(
+        {
+          props: {
+            color: 'red',
+            severity: 'hell',
+          },
+          lastIndex: 1,
+        }
+      )
+    })
+
+    it('trims white space', () => {
+      expect(
+        findProps(['  color  :   red   ', '  severity:    hell   '], 0)
+      ).toEqual(
+        {
+          props: {
+            color: 'red',
+            severity: 'hell',
+          },
+          lastIndex: 1,
+        }
+      )
+    })
+
+    it('ignores invalid lines', () => {
+      expect(
+        findProps(['', 'color: red', 'ignore me!', 'severity: hell', ''], 0)
+      ).toEqual(
+        {
+          props: {
+            color: 'red',
+            severity: 'hell',
+          },
+          lastIndex: 4,
+        }
+      )
+    })
+
+    it('stops at the next heading', () => {
+      expect(
+        findProps(['color: red', '# header', 'severity: hell'], 0)
+      ).toEqual(
+        {
+          props: {
+            color: 'red',
+          },
+          lastIndex: 0,
+        }
+      )
+
+      expect(
+        findProps(['color: red', '## header', 'severity: hell'], 0)
+      ).toEqual(
+        {
+          props: {
+            color: 'red',
+          },
+          lastIndex: 0,
+        }
+      )
+
+      expect(
+        findProps(['color: red', '    ##    header #', 'severity: hell'], 0)
+      ).toEqual(
+        {
+          props: {
+            color: 'red',
+          },
+          lastIndex: 0,
+        }
+      )
+    })
+  })
+
+  describe('findSections', () => {
+    it('finds sections', () => {
+      expect(
+        findSections(`## Hey there! {hey}
+relatively empty
+
+## And another one {another}
+[with placeholder]
+
+Here is some body text!
+
+And some more
+
+# with headings
+__and so on__
+`.split('\n'), 0)
+      ).toEqual(
+        {
+          sections: {
+            hey: {
+              header: 'Hey there!',
+              placeholder: '',
+              body: 'relatively empty',
+            },
+            another: {
+              header: 'And another one',
+              placeholder: 'with placeholder',
+              body: `Here is some body text!
+
+And some more
+
+# with headings
+__and so on__`,
+            },
+          },
+        }
+      )
+    })
+  })
+
+  describe('findPlaceholderText', () => {
+    it('finds placeholder text', () => {
+      expect(
+        findPlaceholderText(['[Hey there!]'], 0)
+      ).toEqual(
+        {
+          placeholder: 'Hey there!',
+          index: 0,
+        }
+      )
+    })
+
+    it('ignores trailing empty lines', () => {
+      expect(
+        findPlaceholderText(['', '   ', '[Hey there!]', 'to be ignored'], 0)
+      ).toEqual(
+        {
+          placeholder: 'Hey there!',
+          index: 2,
+        }
+      )
+    })
+
+    it('ignores trailing text', () => {
+      expect(
+        findPlaceholderText(['ignore me! [Hey there!] ignore me!'], 0)
+      ).toEqual(
+        {
+          placeholder: 'Hey there!',
+          index: 0,
+        }
+      )
+    })
+  })
+})
