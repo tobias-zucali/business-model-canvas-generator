4c9902b10849656babe183f16c411942a9a26b5c
Tobias Zucali <tobias@zucali.at>
2019-03-06 21:29:43 +0100

    utils/array: - introduce mergeArraysByKey

--- src/utils/array/index.js
+++ src/utils/array/index.js
@@ -1,5 +1,8 @@
+import { mergeObjects } from 'utils/object'
+
 import isArray from 'lodash/isArray'
 import isNil from 'lodash/isNil'
+import union from 'lodash/union'
 
 
 export function forceArray(value) {
@@ -9,5 +12,28 @@ export function forceArray(value) {
     return [value]
   } else {
     return []
+  }
+}
+
+export function getArrayObjectKeys(array, key) {
+  return array.map((entry) => entry && entry[key])
+}
+
+export function mergeArraysByKey(destination, source, { key }) {
+  if (isArray(destination) && isArray(source)) {
+    const destinationKeyValues = getArrayObjectKeys(destination, key)
+    const sourceKeyValues = getArrayObjectKeys(source, key)
+    const mergedKeyValues = union(destinationKeyValues, sourceKeyValues)
+
+    return mergedKeyValues.map((keyValue) => {
+      const destinationEntry = destination[destinationKeyValues.indexOf(keyValue)]
+      const sourceEntry = source[sourceKeyValues.indexOf(keyValue)]
+
+      return mergeObjects(destinationEntry, sourceEntry)
+    })
+  } else if (isNil(source)) {
+    return destination
+  } else {
+    return source
   }
 }
--- src/utils/array/tests/index.test.js
+++ src/utils/array/tests/index.test.js
@@ -1,4 +1,4 @@
-import { forceArray } from '../index'
+import { forceArray, mergeArraysByKey } from '../index'
 
 
 describe('utils/array', () => {
@@ -49,6 +49,65 @@ describe('utils/array', () => {
         forceArray()
       ).toEqual(
         []
+      )
+    })
+  })
+
+  describe('mergeArraysByKey', () => {
+    it('merges arrays', () => {
+      const source = [
+        { id: 'a', destination: true },
+        { id: 'b', destination: true },
+      ]
+      const destination = [
+        { id: 'b', source: true },
+        { id: 'c', source: true },
+      ]
+
+      expect(
+        mergeArraysByKey(source, destination, { key: 'id' })
+      ).toEqual([
+        { id: 'a', destination: true },
+        { id: 'b', destination: true, source: true },
+        { id: 'c', source: true },
+      ])
+    })
+
+    it('returns destination if source is no array or nil', () => {
+      const destination = [{ id: 'a' }]
+
+      expect(
+        mergeArraysByKey('hugo', destination, { key: 'id' })
+      ).toEqual(
+        destination
+      )
+
+      expect(
+        mergeArraysByKey(null, destination, { key: 'id' })
+      ).toEqual(
+        destination
+      )
+
+      expect(
+        mergeArraysByKey(undefined, destination, { key: 'id' })
+      ).toEqual(
+        destination
+      )
+    })
+
+    it('returns source if source is nil', () => {
+      const source = [{ id: 'a' }]
+
+      expect(
+        mergeArraysByKey(source, null, { key: 'id' })
+      ).toEqual(
+        source
+      )
+
+      expect(
+        mergeArraysByKey(source, undefined, { key: 'id' })
+      ).toEqual(
+        source
       )
     })
   })
