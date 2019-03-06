488b8575cb9f4fb584a08050e709a3767cd9b87b
Tobias Zucali <tobias@zucali.at>
2019-03-06 21:38:09 +0100

    CanvasArea - add borders

--- src/components/CanvasArea/index.js
+++ src/components/CanvasArea/index.js
@@ -6,10 +6,43 @@ import Editor from 'components/Editor'
 
 
 const AreaBox = styled.div`
-  border: solid 1px;
+  position: relative;
+`
+
+const AreaBorder = styled.div`
+  position: absolute;
+  background-color: #EEEEEE;
+  background-color: ${(props) => props.theme.pageBackground};
+`
+
+const BORDER_GAP = '0.5em'
+const AreaBorderTop = styled(AreaBorder)`
+  height: 1px;
+  left: ${BORDER_GAP};
+  right: ${BORDER_GAP};
+  top: 0;
+`
+const AreaBorderRight = styled(AreaBorder)`
+  bottom: ${BORDER_GAP};
+  right: 0;
+  top: ${BORDER_GAP};
+  width: 1px;
+`
+const AreaBorderBottom = styled(AreaBorder)`
+  bottom: 0;
+  height: 1px;
+  left: ${BORDER_GAP};
+  right: ${BORDER_GAP};
+`
+const AreaBorderLeft = styled(AreaBorder)`
+  bottom: ${BORDER_GAP};
+  left: 0;
+  top: ${BORDER_GAP};
+  width: 1px;
 `
 
 function CanvasArea({
+  border,
   content,
   editorState,
   header,
@@ -17,6 +50,13 @@ function CanvasArea({
   placeholder,
   ...otherProps
 }) {
+  const [
+    hasBorderTop,
+    hasBorderRight,
+    hasBorderBottom,
+    hasBorderLeft,
+  ] = border
+
   return (
     <AreaBox
       {...otherProps}
@@ -31,16 +71,25 @@ function CanvasArea({
         }}
         placeholder={placeholder}
       />
+      {hasBorderTop && <AreaBorderTop />}
+      {hasBorderRight && <AreaBorderRight />}
+      {hasBorderBottom && <AreaBorderBottom />}
+      {hasBorderLeft && <AreaBorderLeft />}
     </AreaBox>
   )
 }
 
 CanvasArea.propTypes = {
+  border: PropTypes.arrayOf(PropTypes.bool.isRequired),
   content: PropTypes.string.isRequired,
   editorState: PropTypes.object,
   header: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   placeholder: PropTypes.string,
+}
+
+CanvasArea.defaultProps = {
+  border: [false, false, false, false],
 }
 
 export default CanvasArea
