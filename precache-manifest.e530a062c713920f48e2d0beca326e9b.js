9f7cac4336938439b4d5a2284802ced6c5a6a4c8
Tobias Zucali <tobias@zucali.at>
2019-03-08 13:05:48 +0100

    Updates

---
+++ precache-manifest.e530a062c713920f48e2d0beca326e9b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "33a744851c21d0235667b75bb9f81cd7",
+    "url": "/business-model-canvas/static/media/formatItalic.33a74485.svg"
+  },
+  {
+    "revision": "a72fcd227da0aae7d48c",
+    "url": "/business-model-canvas/static/css/main.01ae6a58.chunk.css"
+  },
+  {
+    "revision": "a40a6b25127c2dd9c537",
+    "url": "/business-model-canvas/static/js/runtime~main.a40a6b25.js"
+  },
+  {
+    "revision": "4725a735f0f9ac4902c65b97fcf11d8e",
+    "url": "/business-model-canvas/static/media/formatUnderlined.4725a735.svg"
+  },
+  {
+    "revision": "79ede209684a458e6a01",
+    "url": "/business-model-canvas/static/js/2.79ede209.chunk.js"
+  },
+  {
+    "revision": "8680cb7c543e5aae34b57fe39bf7e7d8",
+    "url": "/business-model-canvas/static/media/formatBold.8680cb7c.svg"
+  },
+  {
+    "revision": "e53198a5519022b4a5146fc88b1a7677",
+    "url": "/business-model-canvas/static/media/formatSize.e53198a5.svg"
+  },
+  {
+    "revision": "a72fcd227da0aae7d48c",
+    "url": "/business-model-canvas/static/js/main.a72fcd22.chunk.js"
+  },
+  {
+    "revision": "421c3226481d4bc3b7c9b2438b3a8e38",
+    "url": "/business-model-canvas/static/media/formatListBulleted.421c3226.svg"
+  },
+  {
+    "revision": "84ff2b736c3f490d52a9062e22d4020a",
+    "url": "/business-model-canvas/static/media/formatListNumbered.84ff2b73.svg"
+  },
+  {
+    "revision": "6743ecc4779e0096efcfd55d295bad4d",
+    "url": "/business-model-canvas/static/media/formatQuote.6743ecc4.svg"
+  },
+  {
+    "revision": "ea62c122ec34200053a46df1fbc8bfb8",
+    "url": "/business-model-canvas/static/media/formatStrikethrough.ea62c122.svg"
+  },
+  {
+    "revision": "79ede209684a458e6a01",
+    "url": "/business-model-canvas/static/css/2.f8ca4a7b.chunk.css"
+  },
+  {
+    "revision": "2611c626c44d92377a65476070de4cdc",
+    "url": "/business-model-canvas/index.html"
+  }
+];
