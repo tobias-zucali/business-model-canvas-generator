0fec82c77e7f181d60632071326495463f9475e6
Tobias Zucali <tobias@zucali.at>
2019-06-06 07:56:27 +0200

    Updates

---
+++ precache-manifest.b45b7294eec2acb51f1b275c3aa6ab0a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40acfeef6a994c8edcb2580252a7765a",
+    "url": "/business-model-canvas-generator/index.html"
+  },
+  {
+    "revision": "c4d3e01ac63524fd7ab6",
+    "url": "/business-model-canvas-generator/static/css/2.a17ebd6e.chunk.css"
+  },
+  {
+    "revision": "76baeb823c08c2b52880",
+    "url": "/business-model-canvas-generator/static/css/3.764ccc25.chunk.css"
+  },
+  {
+    "revision": "a641a8c4eccf0c276ded",
+    "url": "/business-model-canvas-generator/static/css/main.36416e79.chunk.css"
+  },
+  {
+    "revision": "c4d3e01ac63524fd7ab6",
+    "url": "/business-model-canvas-generator/static/js/2.492c75ae.chunk.js"
+  },
+  {
+    "revision": "76baeb823c08c2b52880",
+    "url": "/business-model-canvas-generator/static/js/3.f8ec34ca.chunk.js"
+  },
+  {
+    "revision": "b7e3dab2f55bac6c4d4c",
+    "url": "/business-model-canvas-generator/static/js/4.72724fbf.chunk.js"
+  },
+  {
+    "revision": "a641a8c4eccf0c276ded",
+    "url": "/business-model-canvas-generator/static/js/main.95edc101.chunk.js"
+  },
+  {
+    "revision": "cb141f869213fdd4e220",
+    "url": "/business-model-canvas-generator/static/js/runtime~main.d8b684b2.js"
+  },
+  {
+    "revision": "8680cb7c543e5aae34b57fe39bf7e7d8",
+    "url": "/business-model-canvas-generator/static/media/formatBold.8680cb7c.svg"
+  },
+  {
+    "revision": "33a744851c21d0235667b75bb9f81cd7",
+    "url": "/business-model-canvas-generator/static/media/formatItalic.33a74485.svg"
+  },
+  {
+    "revision": "421c3226481d4bc3b7c9b2438b3a8e38",
+    "url": "/business-model-canvas-generator/static/media/formatListBulleted.421c3226.svg"
+  },
+  {
+    "revision": "84ff2b736c3f490d52a9062e22d4020a",
+    "url": "/business-model-canvas-generator/static/media/formatListNumbered.84ff2b73.svg"
+  },
+  {
+    "revision": "e53198a5519022b4a5146fc88b1a7677",
+    "url": "/business-model-canvas-generator/static/media/formatSize.e53198a5.svg"
+  },
+  {
+    "revision": "ea62c122ec34200053a46df1fbc8bfb8",
+    "url": "/business-model-canvas-generator/static/media/formatStrikethrough.ea62c122.svg"
+  },
+  {
+    "revision": "4725a735f0f9ac4902c65b97fcf11d8e",
+    "url": "/business-model-canvas-generator/static/media/formatUnderlined.4725a735.svg"
+  },
+  {
+    "revision": "e8e466e6c688e73d59ff69a6b6b36155",
+    "url": "/business-model-canvas-generator/static/media/note.e8e466e6.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/business-model-canvas-generator/precache-manifest.aa51f00f64d7e78953858b1ff3d805f0.js"
+  "/business-model-canvas-generator/precache-manifest.b45b7294eec2acb51f1b275c3aa6ab0a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/business-model-canvas-generator/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/business-model-canvas-generator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
